ce44908e345906a64269c653a8e6851d9880378f
Adam <adamjawor@gmail.com>
2024-02-29 21:42:34 +0100

    Create basic  dockerfile with docker-compose

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ app.include_router(auth.router, prefix="")
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
