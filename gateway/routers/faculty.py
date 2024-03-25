1c8729611b320d20684bb08e8d025f09805cef3d
Adam <adamjawor@gmail.com>
2024-03-25 13:42:39 +0100

    add sample connetion to fh-faculty-app

--- gateway/routers/faculty.py
+++ gateway/routers/faculty.py
@@ -1,3 +1,15 @@
 """
 APP Faculty for handling Lectures, Subjects and hours for each subject
 """
+import requests
+from fastapi import APIRouter, Depends
+from routers.blocking import ProtectedEndpoint
+
+router = APIRouter()
+
+
+@router.get("/get_subjects", dependencies=[Depends(ProtectedEndpoint)])
+def get_subjects():
+    api_url = "http://127.0.0.1:8100/faculty"
+    all_subjects = requests.get(api_url).json()
+    return {"subjects": all_subjects}
