9074f409a86a79aba1d53ff564ebadc387cfbfce
Adam <33165258+adamj2k@users.noreply.github.com>
2024-03-25 19:55:56 +0100

    add sample connection to fh-faculty-app (#8)
    * add sample connetion to fh-faculty-app
    
    * add configuration for docker

--- gateway/main.py
+++ gateway/main.py
@@ -1,17 +1,31 @@
 import settings
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import HTTPBearer
-from routers import auth
+from routers import auth, faculty
 from starlette.middleware.sessions import SessionMiddleware
 
 token_auth_scheme = HTTPBearer()
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:8100",
+    "http://localhost:8000",
+]
+
 app.add_middleware(SessionMiddleware, secret_key=settings.AUTH0_SESSION_SECRET)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="")
+app.include_router(faculty.router, prefix="/faculty")
 
 
 if __name__ == "__main__":
--- gateway/routers/faculty.py
+++ gateway/routers/faculty.py
@@ -1,3 +1,16 @@
 """
 APP Faculty for handling Lectures, Subjects and hours for each subject
 """
+import requests
+import settings
+from fastapi import APIRouter, Depends
+from routers.blocking import ProtectedEndpoint
+
+router = APIRouter()
+
+
+@router.get("/get_subjects", dependencies=[Depends(ProtectedEndpoint)])
+def get_subjects():
+    api_url = f"http://{settings.FH_APP_FACULTY_URL}/faculty"
+    all_subjects = requests.get(api_url).json()
+    return {"subjects": all_subjects}
