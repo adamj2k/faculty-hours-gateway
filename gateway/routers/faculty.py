18a1954318865d8fb4031fe1f7d08b534abc23c0
Adam <adamjawor@gmail.com>
2024-03-25 19:54:05 +0100

    add configuration for docker

--- gateway/routers/faculty.py
+++ gateway/routers/faculty.py
@@ -2,6 +2,7 @@
 APP Faculty for handling Lectures, Subjects and hours for each subject
 """
 import requests
+import settings
 from fastapi import APIRouter, Depends
 from routers.blocking import ProtectedEndpoint
 
@@ -10,6 +11,6 @@ router = APIRouter()
 
 @router.get("/get_subjects", dependencies=[Depends(ProtectedEndpoint)])
 def get_subjects():
-    api_url = "http://127.0.0.1:8100/faculty"
+    api_url = f"http://{settings.FH_APP_FACULTY_URL}/faculty"
     all_subjects = requests.get(api_url).json()
     return {"subjects": all_subjects}
