dbb38c3d060485317b6cddd1c019f8183dabd6bf
Adam <adamjawor@gmail.com>
2024-04-04 15:34:50 +0200

    Add basic token verifcation

--- gateway/routers/faculty.py
+++ gateway/routers/faculty.py
@@ -3,14 +3,15 @@ APP Faculty for handling Lectures, Subjects and hours for each subject
 """
 import requests
 import settings
-from fastapi import APIRouter, Depends
-from routers.blocking import ProtectedEndpoint
+from fastapi import APIRouter, Depends, Security
+from routers.blocking import ProtectedEndpoint, VerifyToken
 
 router = APIRouter()
+auth = VerifyToken()
 
 
 @router.get("/get_subjects", dependencies=[Depends(ProtectedEndpoint)])
-def get_subjects():
+async def get_subjects(auth_result: str = Security(auth.verify)):
     api_url = f"http://{settings.FH_APP_FACULTY_URL}/faculty"
     all_subjects = requests.get(api_url).json()
     return {"subjects": all_subjects}
