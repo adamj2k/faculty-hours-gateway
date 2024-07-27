f54e118fb8500896f0cdfb260067caee51ab449f
Adam <adamjawor@gmail.com>
2024-07-27 22:11:21 +0200

    added teacher report list to get it from report service

--- gateway/routers/report.py
+++ gateway/routers/report.py
@@ -1,8 +1,17 @@
 """
 APP Report for generating reports
 """
-from fastapi import APIRouter
+import requests
+import settings
+from fastapi import APIRouter, Security
 from routers.blocking import VerifyToken
 
 router = APIRouter()
 auth = VerifyToken()
+
+
+@router.get("/teacher-reports/list")
+async def get_subjects(auth_result: str = Security(auth.verify)):
+    api_url = f"http://{settings.FH_APP_REPORT_URL}/teacher-reports/list"
+    teacher_report_list = requests.get(api_url).json()
+    return teacher_report_list
